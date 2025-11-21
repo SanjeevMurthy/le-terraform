1f7033cb7edc8af4839f23dc348f193b4b772d29
Sanjeev Murthy <sanjeevmurthy01@gmail.com>
2025-11-21 19:48:16 +0530

    add azure VM module

---
+++ Azure/modules/vm/main.tf
@@ -0,0 +1,28 @@
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group
+  size                = var.vm_size
+  admin_username      = var.admin_username
+
+  network_interface_ids = [
+    var.nic_id
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.ssh_public_key)
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
---
+++ Azure/modules/vm/variables.tf
@@ -0,0 +1,7 @@
+variable "name" {}
+variable "location" {}
+variable "resource_group" {}
+variable "nic_id" {}
+variable "vm_size" {}
+variable "admin_username" {}
+variable "ssh_public_key" {}
