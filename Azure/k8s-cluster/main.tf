552cddc0cfeaf93207e851ea861305639a6158aa
SanjeevMurthy <sanjeevmurthy01@gmail.com>
2025-11-24 22:24:55 +0530

    associate nsg with subnet

--- Azure/k8s-cluster/main.tf
+++ Azure/k8s-cluster/main.tf
@@ -59,6 +59,13 @@ resource "azurerm_network_security_group" "nsg" {
   }
 }
 
+# Associate NSG with subnet
+resource "azurerm_subnet_network_security_group_association" "nsg_association" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+
 
 
 # -------------------------------
