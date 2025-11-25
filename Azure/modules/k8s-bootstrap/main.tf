d17341b29328be956bc441350f03382e88b91472
SanjeevMurthy <sanjeevmurthy01@gmail.com>
2025-11-25 17:28:40 +0530

    update cloud init script

--- Azure/modules/k8s-bootstrap/main.tf
+++ Azure/modules/k8s-bootstrap/main.tf
@@ -16,8 +16,9 @@ locals {
     systemctl enable containerd || true
 
     # Install Kubernetes apt repo and packages
-    curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-    echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] http://apt.kubernetes.io/ kubernetes-xenial main" >/etc/apt/sources.list.d/kubernetes.list
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+    echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
     apt-get update -y
     apt-get install -y kubelet kubeadm kubectl
